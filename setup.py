583f57b599ca62cf0465a4ec04b7d6f437593c06
Nihar Mittal <niharmittal9185@gmail.com>
2024-08-05 16:45:32 +0530

    requirements aded

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages , setup
+setup(
+    name = 'RAG',
+    version = '0.0.0',
+    author = 'Nihar',
+    author_email= 'niharmittal9185@gmail.com',
+    packages= find_packages(),
+    install_requires=[]
+)
