0a209c450500ea084cdfb4111fe7bd977beb6bc5
Nihar Mittal <niharmittal9185@gmail.com>
2024-08-05 20:01:20 +0530

    final

--- app.py
+++ app.py
@@ -1,40 +1,51 @@
 import streamlit as st
-from src.helper import get_pdf_text , get_text_chunks , get_vector_stores, get_conversational_chain
+from src.helper import get_pdf_text , get_text_chunks , get_vector_store, get_conversational_chain
+from langchain.vectorstores import FAISS
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
+os.environ["GOOGLE_API_KEY"]=GOOGLE_API_KEY
+
 
 def user_input(user_question):
-    response= st.session_state.conversation({'question': user_question})
-    st.session_state.chatHistory=response['chat_history']
-    for i,message in enumerate(st.session_state.chatHistory):
-        if i%2==0:
-            st.write("User: ", message.content)
-        else:
-            st.write("Reply: ", message.content)
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
 
             
 def main():
-    st.set_page_config("INFORMATION RETREIVAL")
-    st.header("RAG MODEL")
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using Gemini💁")
 
-    user_question=st.text_input("Ask a Question from the PDF files")
+    user_question = st.text_input("Ask a Question from the PDF Files")
 
-    if "conversation" not in st.session_state:
-        st.session_state.conversation=None
-    if "chatHistory" not in st.session_state:
-        st.session_state.chatHistory=None
     if user_question:
         user_input(user_question)
 
     with st.sidebar:
         st.title("Menu:")
-        pdf_docs=st.file_uploader("Upload your PDF files and click on the submit & Process Button" , accept_multiple_files=True)
-        if st.button("Submit and Process"):
-            with st.spinner("Processing.."):
-                raw_text=get_pdf_text(pdf_docs)
-                text_chunks=get_text_chunks(raw_text)
-                vector_store=get_vector_stores(text_chunks)
-                st.session_state.conversation = get_conversational_chain(vector_store)
-                
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
                 st.success("Done")
+
 
 
 if __name__ == "__main__":
--- src/helper.py
+++ src/helper.py
@@ -1,11 +1,11 @@
 import os
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import GooglePalmEmbeddings
-from langchain.llms import google_palm
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -22,19 +22,34 @@ def get_pdf_text(pdf_docs):
 
 
 def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000 , chunk_overlap=20)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000 , chunk_overlap=1000)
     chunks=text_splitter.split_text(text)
     return chunks
 
 
-def get_vector_stores(text_chunks):
-    embeddings=GooglePalmEmbeddings()
-    vector_store= FAISS.from_texts(text_chunks,embedding=embeddings)
-    return vector_store
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
 
-def get_conversational_chain(vector_store):
-    llm=google_palm()
-    memory= ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-    conversation_chain=ConversationalRetrievalChain.from_llm(llm=llm , retreiver=vector_store.as_retreiver() ,memory=memory)
-    return conversation_chain
+
+def get_conversational_chain():
+
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                             temperature=0.3)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+
 
